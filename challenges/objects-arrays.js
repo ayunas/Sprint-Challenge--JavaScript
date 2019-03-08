8899b37866c0e752b501c2a91749c21dc7a8fc90
ayunas <ayunas@gmail.com>
2019-03-08 10:40:29 -0600

    pushed the animal common and scientific names to an array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -127,12 +127,16 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
 {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
+
 /* Request 1: .forEach()
 
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
 const animalNames = [];
+
+zooAnimals.forEach( (animal,i) => animalNames.push('Name: ' + zooAnimals[i].animal_name,'Scientific: ' + zooAnimals[i].scientific_name));
+
 console.log(animalNames);
 
 /* Request 2: .map()    
